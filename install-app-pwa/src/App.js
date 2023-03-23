71353292586932dbfae5ca180e8ce21b3e777e31
Ede Harrison <ede_harrison@Edes-MacBook-Pro.local>
2023-03-23 15:38:34 +0000

    set up notes added to App.js

--- install-app-pwa/src/App.js
+++ install-app-pwa/src/App.js
@@ -1,15 +1,25 @@
 // Set up instructions
 // 
-// 1) npx create-react-app . --template cra-template-pwa 
+// 1) npx create-react-app . --template cra-template-pwa
+// (we will do this via a typescript-specific command when we use typescript. The above command is for just plain JS+React) 
 // 2) We need to opt-in to enable service workers. For this, in the index.js: 
 // Change:
 // serviceWorkerRegistration.unregister();
 // To:
 // serviceWorkerRegistration.register();
 // - In index.js:
-// 3) declare a deferredPrompt variable (l8)
-// 4) add an event listener to window, 
+// 3) The code we wrote / altered was:
+// App.js l24 - 42
+// Index.js l18 (serviceWorkerRegistration.register())
 
+// For more instructions, see related Trello > Spike ticket
+
+// To run / test in browser:
+
+// npm run build
+// follow npm’s instructions after that (it will be one or two more commands)
+// go to browser, install app via button / clicking icon in right of url bar (in Chrome, at least)
+// You have to uninstall and build each time if you want to test install more than once. To uninstall app (in Chrome), go on the app > top right 3 dots (more) > click 'uninstall <app name>
 
 // Useful reading:
 // React-specific setup of PWAs: https://create-react-app.dev/docs/making-a-progressive-web-app/
